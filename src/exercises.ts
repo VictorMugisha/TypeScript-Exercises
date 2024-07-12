f505fed822280e4df6e388f9e7f867e14db773f0
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-12 08:21:02 +0200

    Updated the example with never type

--- src/exercises.ts
+++ src/exercises.ts
@@ -29,6 +29,33 @@ function isItOdd(value: number): boolean  {
 
  
 
+// type Props = {
+//     name: string
+// } & ({
+//     gender: 'male'
+//     salary: number
+// } | {
+//     gender: 'female'
+//     weight: number
+// })
+
+
+// function child(args: Props) {
+//     if ('salary' in args) {
+//         console.log("Salary is: ", args.salary)
+//     } 
+//     if ('weight' in args) {
+//         console.log("Weight is: ", args.weight)
+//     }
+// }
+
+
+// child({ gender: 'male', name: 'John', salary: 200000})
+// child({ gender: 'female', name: 'John', weight: 78})
+
+
+
+// We can also use exhaustiveness checking with never variable
 type Props = {
     name: string
 } & ({
@@ -41,14 +68,20 @@ type Props = {
 
 
 function child(args: Props) {
-    if ('salary' in args) {
-        console.log("Salary is: ", args.salary)
-    } 
-    if ('weight' in args) {
-        console.log("Weight is: ", args.weight)
+    if (args.gender === "male") {
+        console.log(args.salary)
+    } else if (args.gender === "female") {
+        console.log(args.weight)
+    } else {
+        const doomed: never = args
+        failed(doomed);
     }
 }
 
+function failed(value: never): never {
+    throw new Error(`${value}, is not a thing`)
+}
 
 child({ gender: 'male', name: 'John', salary: 200000})
 child({ gender: 'female', name: 'John', weight: 78})
+child({ gender: 'something', name: 'John', weight: 78})
